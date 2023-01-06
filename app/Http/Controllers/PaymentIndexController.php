6e30c3ebda59a2fe8292cbd3f42bff67a83c4513
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-01-06 18:14:46 +0630

    prevent creating multiple payment intents with session

--- app/Http/Controllers/PaymentIndexController.php
+++ app/Http/Controllers/PaymentIndexController.php
@@ -8,16 +8,20 @@ class PaymentIndexController extends Controller
 {
     public function __invoke()
     {
-        //1 - create a payment intent
-
-        $paymentIntent = app('stripe')->paymentIntents->create([
-            'amount' => 10000, //100 usd
-            'currency' => 'usd',
-            'setup_future_usage' => 'on_session',
-            'metadata' => [
-                'user_id' => auth()->user()->id
-            ]
-        ]);
+        //1 - if payment intent exists -> use it, if not, create a payment intent and store in the session
+        if (session()->has('payment_intent_id')) {
+            $paymentIntent = app('stripe')->paymentIntents->retrieve(session()->get('payment_intent_id'));
+        } else {
+            $paymentIntent = app('stripe')->paymentIntents->create([
+                'amount' => 10000, //100 usd
+                'currency' => 'usd',
+                'setup_future_usage' => 'on_session',
+                'metadata' => [
+                    'user_id' => auth()->user()->id
+                ]
+            ]);
+            session()->put('payment_intent_id', $paymentIntent->id);
+        }
 
         return view('payments', [
             'payment_intent' => $paymentIntent
